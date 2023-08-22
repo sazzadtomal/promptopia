690aab030e76bbd7606c1810355e468d5e6e055e
sazzadtomal <sazzadtomalfordesktop1660@gmail.com>
2023-08-22 17:43:49 +0600

    added layout, home and nav component

---
+++ components/Nav.jsx
@@ -0,0 +1,104 @@
+"use client";
+
+import Link from "next/link"
+import Image from "next/image"
+import { useState,useEffect } from "react"
+import {signIn,signOut,useSession, getProviders} from "next-auth/react"
+
+const Nav = () => {
+    const [providers,setProviders]=useState(null);
+    const [toggleDropDown,setToggleDropDown]=useState(false)
+    const isUserLoggedin=true;
+
+
+    // useEffect(()=>{
+    //     const setProviders= async ()=>{
+    //         const response= await getProviders();
+    //         setProviders(response)
+    //     }
+
+    //     setProviders()
+    // },[])
+
+
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+         <Link href="/" className="flex gap-2 flex-center" >
+            <Image src="/assets/images/logo.svg" 
+            alt="Promptopia Logo" 
+            width={30}
+            height={30}
+            className="object-contain"/>
+            <p className="logo_text">Propmtopia</p></Link>
+
+
+            <div className="sm:flex hidden" >
+                {isUserLoggedin ? (<div className="flex gap-3 md:gap-5">
+                    <Link href="/create-prompt" className="black_btn">
+                        Create Post
+                    </Link>
+
+                    <button type='button' onClick={signOut} className='outline_btn'>
+                     Sign Out
+                    </button>
+                  <Link href="/profile" >
+                    <Image src="/assets/images/logo.svg" width={37} height={37} className="rounded-full" alt="profile"/>
+                </Link>
+                </div>) : (
+                <>
+                {providers && Object.values(providers).map(provider=>(
+                    <button type="button" key={provider.name} onClick={()=>signIn(provider.id)}>
+                      Sign in
+                    </button>
+
+                ))}
+                
+                </>) }
+
+               
+
+
+            </div>
+
+            {/* Mobile navigation */}
+
+           <div className="sm:hidden flex relative">
+              {isUserLoggedin ? (
+                <div className="flex">
+                  <Image onClick={()=>setToggleDropDown((prev)=>!prev)} src="/assets/images/logo.svg" width={37} height={37} className="rounded-full" alt="profile"/> 
+                   
+                   {toggleDropDown && <div className="dropdown">
+                    <Link href="/profile"
+                    className="dropdown_link" onClick={()=>setToggleDropDown(false)}>
+                      My Profile
+                    </Link>
+                    <Link href="/create-propmt"
+                    className="dropdown_link" onClick={()=>setToggleDropDown(false)}>
+                      Create Prompt
+                    </Link>
+                    <button className="mt-5 w-full black_btn" type="button" onClick={()=>{setToggleDropDown(false),signOut()}} >
+                        Sign Out
+                    </button>
+                   </div>  }
+                </div>
+              ): (
+                <>
+                {providers && Object.values(providers).map(provider=>(
+                    <button type="button" key={provider.name} onClick={()=>signIn(provider.id)}>
+                      Sign in
+                    </button>
+
+                ))}
+                
+                </>)  }
+            </div>
+
+
+         
+            
+
+    </nav>
+  )
+}
+
+export default Nav
