9dfc294ca7c2893f10467c3d0fd4f4462bdca7d6
sazzadtomal <sazzadtomalfordesktop1660@gmail.com>
2023-08-26 17:18:48 +0600

    added all crud operations

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -10,6 +10,14 @@ import { usePathname,useRouter } from "next/navigation"
 
 const PromptCard = ({post,handleTagClick,handleEdit,handleDelete}) => {
    const [copied,setCopied]=useState("")
+
+   const {data:session}=useSession()
+
+   const pathName=usePathname()
+
+   console.log(session?.user.id,pathName )
+
+
    const handleCopy=()=>{
     setCopied(post.prompt)
     navigator.clipboard.writeText(post.prompt)
@@ -35,6 +43,23 @@ const PromptCard = ({post,handleTagClick,handleEdit,handleDelete}) => {
         </div>
           <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
           <p onClick={()=>handleTagClick && handleTagClick(post.tag)} className="font-inter text-sm blue_gradient cursor-pointer">{post.tag}</p>
+
+          {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+          <p
+            className='font-inter text-sm green_gradient cursor-pointer'
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className='font-inter text-sm orange_gradient cursor-pointer'
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
 
 
 
